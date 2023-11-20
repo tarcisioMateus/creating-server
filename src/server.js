38ded5ef0e22e8ee38c281ceb579c51f7fa849f7
Tarcísio Mateus <tarcisiomateus98@gmail.com>
2023-11-20 17:11:18 -0300

    created middlewares/json.js

--- src/server.js
+++ src/server.js
@@ -1,7 +1,11 @@
 import http from 'node:http'
 
-const server = http.createServer( (req, res) => {
-  
+import { json } from './middlewares/json.js'
+
+const server = http.createServer( async (req, res) => {
+  const { method, url } = req
+
+  await json(req, res)
 })
 
 server.listen(3333)
